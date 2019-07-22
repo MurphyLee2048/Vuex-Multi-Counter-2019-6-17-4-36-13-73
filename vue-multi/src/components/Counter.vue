d27387fd0db0e7ac57b8d098de98bf7aebf7324e
murphylee2048 <1134697131@qq.com>
2019-07-22 16:48:47 +0800

    commit CounterGroup

---
+++ vue-multi/src/components/Counter.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <button v-on:click="decrease">-</button>
+    <span>{{count}}</span>
+    <button @click="increase">+</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Counter",
+    data() {
+      return {
+        count: 0
+      }
+    },
+    methods: {
+      increase() {
+        this.count++;
+        this.$store.state.sum++;
+      },
+      decrease() {
+        this.count--;
+        this.$store.state.sum--;
+      },
+    },
+    beforeDestroy: function () {
+      this.$emit('reSum',  this.$store.state.sum - this.count);
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue-multi/src/components/CounterGroup.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <input type="number" v-model.number="number" placeholder="CounterNumber">
+    <Counter v-for="n in number" v-on:reSum="reSum"></Counter>
+    <button>Sum: {{sum}}</button>
+  </div>
+</template>
+
+<script>
+  import Counter from "./Counter.vue"
+  export default {
+    name: "CounterGroup",
+    data() {
+      return {
+        number: 1,
+      }
+    },
+    computed: {
+      sum() {
+        return this.$store.state.sum;
+      }
+    },
+    components: {Counter},
+    methods: {
+      reSum(sum) {
+        this.$store.state.sum = sum;
+      }
+    },
+  }
+</script>
+
+<style scoped>
+</style>
